18079b8a6d39166c2acb94ff26ff0f9fe8d97beb
soumyabrata <soumyabratam6@gmail.com>
2024-10-22 21:06:35 +0530

    all file added...

--- main.js
+++ main.js
@@ -6,3 +6,28 @@ const http = require('http');
 
 // DO NOT EDIT BELOW THIS LINE (Uncomment it once you are done with your code)
 
+const fs = require('fs');
+const path = require('path');
+const filePath = process.argv[2];
+
+if (!filePath) {
+    console.error('Please provide a file path as a command-line argument.');
+    process.exit(1);  
+  }
+
+  const server = http.createServer((req, res) => {
+    fs.readFile(path.resolve(filePath), 'utf-8', (err, data) => {
+      if (err) {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end(`Error reading file: ${err.message}`);
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end(data);
+      }
+    });
+  });
+
+  server.listen(3000, () => {
+    console.log('Server running at http://localhost:3000/');
+  });
+
