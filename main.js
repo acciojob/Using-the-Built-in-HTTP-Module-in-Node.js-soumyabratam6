148a0a8bacf2b825e4e6c247f15e7ac22397cfa8
soumyabrata <soumyabratam6@gmail.com>
2024-10-23 07:41:39 +0530

    all file edit and fix

--- main.js
+++ main.js
@@ -7,27 +7,19 @@ const http = require('http');
 // DO NOT EDIT BELOW THIS LINE (Uncomment it once you are done with your code)
 
 const fs = require('fs');
-const path = require('path');
-const filePath = process.argv[2];
-
-if (!filePath) {
-    console.error('Please provide a file path as a command-line argument.');
-    process.exit(1);  
-  }
-
   const server = http.createServer((req, res) => {
-    fs.readFile(path.resolve(filePath), 'utf-8', (err, data) => {
-      if (err) {
-        res.writeHead(404, { 'Content-Type': 'text/plain' });
-        res.end(`Error reading file: ${err.message}`);
-      } else {
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end(data);
-      }
-    });
+   fs.readFile('output.txt','utf8',(err,data)=>{
+    if (err) {
+      res.writeHead(500,{'Content-Type':'text/plain'});
+      res.end("error reading file")
+    }else{
+      res.writeHead(200,{'content-Type':'text/plain'});
+      res.end(data)
+    }
+   });
   });
 
   server.listen(3000, () => {
-    console.log('Server running at http://localhost:3000/');
-  });
+  console.log('Server running at http://localhost:3000/');
+});
 
