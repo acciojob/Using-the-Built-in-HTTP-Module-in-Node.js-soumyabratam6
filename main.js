4b6f7e2982cb506e4e91d3a67070af8aba5c93c4
soumyabrata <soumyabratam6@gmail.com>
2024-11-04 18:48:26 +0530

    all file added and fix

--- main.js
+++ main.js
@@ -9,12 +9,17 @@ const http = require('http');
 const fs = require('fs');
   const server = http.createServer((req, res) => {
    fs.readFile('output.txt','utf8',(err,data)=>{
-    res.writeHead(200,{'content-Type':'text/plain'});
-    res.end(data)
+    if (err) {
+      res.writeHead(500,{'Content-Type':'text/plain'});
+      res.end("error reading file")
+    }else{
+      res.writeHead(200,{'content-Type':'text/plain'});
+      res.end(data)
+    }
    });
   });
 
-  server.listen(5050, () => {
-  console.log('Server running at http://localhost:5050/');
+  server.listen(3000, () => {
+  console.log('Server running at http://localhost:3000/');
 });
 
